3a73f1020b76ed4602f72ae6cad04dc37ac030d5
Naitik Chattaraj <naitikchattaraj77@gmail.com>
2026-02-23 20:45:24 +0530

    ooh its ready
    Co-authored-by: Naitik Chattaraj <naitikchattaraj41@gmail.com>

---
+++ index.js
@@ -0,0 +1,20 @@
+import jsonfile from "jsonfile";
+import moment from "moment";
+import simpleGit from "simple-git";
+
+const markCommit = (x, y) => {
+const date = moment ()
+. subtract(1, "y")
+.add(1, "d")
+.add(x, "w")
+.add(y, "d")
+. format();
+
+const data = {
+date: date,
+};
+
+jsonfile.writeFile(path, data, () => {
+simpleGit().add([path]).commit(date, {" -- date": date}).push();
+});
+};
