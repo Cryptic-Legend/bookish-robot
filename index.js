afa3a896a4d44bb7d6d900c377fa488fac81aeb1
Cryptic-Legend <naitikchattaraj77@gmail.com>
2026-02-23 21:06:06 +0530

    Added feature
    Co-authored-by: Naitik-Chattaraj  <naitikchattaraj41@gmail.com>

--- index.js
+++ index.js
@@ -2,21 +2,27 @@ import jsonfile from "jsonfile";
 import moment from "moment";
 import simpleGit from "simple-git";
 
+const git = simpleGit();
 const path = "./data.json";
-const markCommit = (x, y) => {
-const date = moment ()
-. subtract(1, "y")
-.add(1, "d")
-.add(x, "w")
-.add(y, "d")
-. format();
 
-const data = {
-date: date,
+const markCommit = async (x, y) => {
+  const date = moment()
+    .subtract(1, "y")
+    .add(1, "d")
+    .add(x, "w")
+    .add(y, "d")
+    .format("YYYY-MM-DDTHH:mm:ss");
+
+  const data = { date };
+
+  console.log("Creating commit for:", date);
+
+  await jsonfile.writeFile(path, data);
+  await git.add(path);
+  await git.commit(date, { "--date": date });
+  await git.push();
+
+  console.log("Commit pushed.");
 };
 
-jsonfile.writeFile(path, data, () => {
-simpleGit().add([path]).commit(date, {"-- date": date}).push();
-});
-};
 markCommit(1, 1);
